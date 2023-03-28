69d82288016744b73b5dc49f4f71f06631a70fd8
MatteoTarabella04 <tarabella.matteo04@gmail.com>
2023-03-28 15:41:38 +0200

    initial commit set stages

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* 
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1 utilizzando una proprietá nell'oggetto restituito dalla funzione data.
+
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+const { createApp } = Vue
+
+createApp({
+   data() {
+      return {
+         message: 'test'
+      }
+   }
+}).mount('#app');
